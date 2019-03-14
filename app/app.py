68c38c64bd4ab7c058712538ebc16b2238e8af6e
Kirill Savine <kirill.savine@gmail.com>
2019-03-14 20:52:20 +0000

    Added Flask API draft setup

--- app/app.py
+++ app/app.py
@@ -1,16 +1,35 @@
-from flask import Flask
 import pandas as pd
 import os
 import socket
 import getpass
 import threading, webbrowser
+
+from flask import Flask, request
+from flask_restful import Resource, Api
 from sqlalchemy.engine import Connection, create_engine, Engine
 
+# con_string = os.getenv("DB_CONN", "not found")  # todo (see how to store this in an infra file)
+con_string = 'postgresql://postgres:postgres@localhost:5432'
+# db_table =  os.getenv("DB_TABLE", "not found")  # todo
+db_table = 'postgres.log'
+
 app = Flask(__name__)
+api = Api(app)
 
 
 def insert_data(df: pd.DataFrame, engine: Engine):
     df.to_sql(name="log", con=engine, index=False, if_exists='append')
+
+def fetch_data(engine: Engine):
+    df.read_sql(f"select * from {db_table}")
+
+class Map(Resource):
+    def get(self, id):
+        return {todo_id: todos[id]}
+
+    def put(self, id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[id]}
 
 
 @app.route("/")
@@ -23,7 +42,7 @@ def hello():
            "<hr> files:<br> {files}"
 
     files = '<br>'.join(os.listdir('.'))
-    con_string = os.getenv("DB_CONN", "not found")
+
     engine = create_engine(con_string)
     df = pd.DataFrame({'user': ['123'], 'x': [1]})
     insert_data(df, engine)
